c29bbac171636ce0a6030917fca45d7f4ce5feda
wsgzg <wsgzg1991@163.com>
2018-07-25 11:12:03 +0800

    can connect to any ethereum node by browser

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ const config = {
   contract: {
     MOECOIN: '0x3b8f4f2376dc9b433606eefa8851deaa134f20c7',
     MOEFUND: '0x377cef524d0442cfefcfa59bf2dd4f4f672e4678',
-    MOECROWD: '0x71a533ba5fe4aca8b0b10db48be5a216ee6a3451'
+    MOECROWD: '0x71a533ba5fe4aca8b0b10db48be5a216ee6a3451',
+    GANACHE_URL: 'http://localhost:7545'
   }
 }
 
